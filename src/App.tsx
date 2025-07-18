89e347d2043494ecc47da7864edcdd661bdcff8b
Jake Weiss <jakeweiss@gmail.com>
2025-07-17 20:55:24 -0500

    c

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({ children }) {
   );
 }
 
-export default function Home() {
+ 
   const [helpModalOpen, setHelpModalOpen] = useState(false);
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-between font-body game-background">
