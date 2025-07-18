697541f0c126cce1b51b41335a9742597e4454c8
Jake Weiss <jakeweiss@gmail.com>
2025-07-17 19:48:31 -0500

    n convexclient.tsx  enter the commit message for your changes. Lines starting

--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -1,14 +1,7 @@
 import { ReactNode } from 'react';
-import { ConvexReactClient, ConvexProvider } from 'convex/react';
-// import { ConvexProviderWithClerk } from 'convex/react-clerk';
-// import { ClerkProvider, useAuth } from '@clerk/clerk-react';
+import { ConvexReactClient } from 'convex/react';
+import { ConvexProviderWithAuth } from 'convex/react-auth';
 
-/**
- * Determines the Convex deployment to use.
- *
- * We perform load balancing on the frontend, by randomly selecting one of the available instances.
- * We use localStorage so that individual users stay on the same instance.
- */
 function convexUrl(): string {
   const url = import.meta.env.VITE_CONVEX_URL as string;
   if (!url) {
@@ -20,11 +13,5 @@ function convexUrl(): string {
 const convex = new ConvexReactClient(convexUrl(), { unsavedChangesWarning: false });
 
 export default function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return (
-    // <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY as string}>
-    // <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-    <ConvexProvider client={convex}>{children}</ConvexProvider>
-    // </ConvexProviderWithClerk>
-    // </ClerkProvider>
-  );
+  return <ConvexProviderWithAuth client={convex}>{children}</ConvexProviderWithAuth>;
 }
